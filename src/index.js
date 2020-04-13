a90f7113846696a785d9faf4214224969f1eebe3
Nicolas de Chevigné <nico2che@gmail.com>
2020-04-13 14:58:12 +0200

    Add redux logger middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import "./index.css";
+import { createStore, applyMiddleware } from "redux";
 import { Switch, Route, Redirect, Router } from "react-router";
 import { createBrowserHistory } from "history";
+import { createLogger } from "redux-logger";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+
+import "./index.css";
 
 import Files from "./containers/Files";
 import Transfers from "./containers/Transfers";
@@ -12,19 +18,25 @@ import TopBar from "./containers/TopBar";
 
 import reducers from "./redux/reducers";
 
+const theme = createMuiTheme();
+
+const logger = createLogger();
 const history = createBrowserHistory();
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(logger));
 
 function App() {
   return (
     <Router history={history}>
       <Provider store={store}>
-        <TopBar />
-        <Switch>
-          <Redirect exact path="/" to="/files" />
-          <Route exact path="/files" component={Files} />
-          <Route exact path="/transfers" component={Transfers} />
-        </Switch>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <TopBar />
+          <Switch>
+            <Redirect exact path="/" to="/files" />
+            <Route exact path="/files" component={Files} />
+            <Route exact path="/transfers" component={Transfers} />
+          </Switch>
+        </ThemeProvider>
       </Provider>
     </Router>
   );
